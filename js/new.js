06e1e2396e02541e0073ce6fe48926d5c178a914
unknown <pavithrasivanoli@gmail.com>
2022-12-16 22:18:33 +0530

    updated the files

---
+++ js/new.js
@@ -0,0 +1,44 @@
+//even in for loop
+//var i=0;
+/*var a=parseInt(prompt('enter'));
+for (i=a;i>=2;i=i-2)
+{
+	console.log(i)
+}*/
+/*//odd in for loop
+for (i=199;i>=100;i=i-2)
+{
+	console.log(i)
+}*/
+//odd in while loop
+/*i=199;
+while(i>=100)
+{
+	console.log(i)
+	i=i-2;
+}*/
+//even in while loop
+/*var i=200
+do
+{
+	console.log(i);
+i=i-2;
+}
+while(i>=100);
+*/
+/*var i=199
+do
+{
+	console.log(i);
+i=i-2;
+}
+while(i>=100);*/
+
+/*var a = parseInt(prompt("Enter the value"));
+for(let i = 0; i<=a ; i++)
+{
+  if(i%2!==0)
+  {
+    console.log("number"+" "+ i);
+  }
+}*/
